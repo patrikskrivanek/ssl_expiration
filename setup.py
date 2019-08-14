e6ec0fb3aef4fbf32dff0dc47fd34fbb28ad066f
Patrik Skřivánek <patrik.skrivanek@gmail.com>
2019-08-14 19:44:22 +0200

    added basic setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'ssl_expiration',
+    packages = ['ssl_expiration'],
+    version = 'v0.0.0',
+    description = 'Python tool for checking domain ssl certificate',
+    author = 'Patrik Skřivánek',
+    author_email = 'kriegsmarine1995@gmail.com',
+    url = 'https://github.com/patrikskrivanek/ssl_expiration.git',
+    download_url = 'https://github.com/patrikskrivanek/ssl_expiration/archive/v0.0.0.tar.gz',
+    keywords = ['ssl', 'check', 'certificate'],
+    classifiers = [],
+)
