fca132fd338b905b5709f5e60326745b49808947
Patrik Skřivánek <patrik.skrivanek@gmail.com>
2019-08-17 11:29:06 +0200

    - exception fix (connection refused - socket)  - setup update

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='sslexp',
-    version='v1.0.0',
+    name="sslexp",
+    version="v1.0.0",
     scripts=['sslexp'],
     author="Patrik Skřivánek",
     author_email="kriegsmarine1995@gmail.com",
@@ -13,10 +13,14 @@ setuptools.setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/patrikskrivanek/ssl_expiration",
+    download_url="https://github.com/patrikskrivanek/ssl_expiration/releases",
     packages=setuptools.find_packages(),
+    license="MIT",
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    keywords="ssl certificate check"
 )
